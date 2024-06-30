2a2467b86788d24e512cb9325fe8077fdf32336b
naisho <vertigodragon@gmail.com>
2024-06-29 23:59:11 -0400

    added SWUDB workarounds

--- src/app/api/booster/swudb.ts
+++ src/app/api/booster/swudb.ts
@@ -1,8 +1,43 @@
-import { sortByAspectRarityCostId } from "@/app/sort";
 import { Expansion } from "@/types/card/attributes/Expansion";
 import { Type } from "@/types/card/attributes/Type";
 import { Card } from "@/types/card/Card";
 import { SWUDBCardEntry, SWUDBDeck } from "@/types/swudb/SWUDBDeck";
+
+function getSORReprint(cardNumber: number): string {
+    return (
+        {
+            66: "SOR_068", // Cargo Juggernaut
+            238: "SOR_229", // Cell Block Guard
+            30: "SOR_033", // Death Trooper
+            166: "SOR_162", // Disabling Fang Fighter
+            81: "SOR_080", // General Tagge
+            121: "SOR_117", // Mercenary Company
+            70: "SOR_069", // Resilient
+            83: "SOR_081", // Seasoned Shoretrooper
+            223: "SOR_215", // Snapshot Reflexes
+            236: "SOR_227", // Snowtrooper Lieutenant
+            85: "SOR_083", // Superlaser Technician
+            231: "SOR_220", // Surprise Strike
+            63: "SOR_066", // System Patrol Craft
+            257: "SOR_247", // Underworld Thug
+        }[cardNumber] ?? ""
+    );
+}
+
+function getSWUDBAlternative(cardNumber: number): string {
+    return (
+        {
+            19: "SOR_020", // Remnant Science Facility
+            20: "SOR_021", // Remote Village
+            21: "SOR_023", // Maz Kanata’s Castle
+            22: "SOR_024", // Nevarro City
+            23: "SOR_026", // Death Watch Hideout
+            24: "SOR_027", // Spice Mines
+            25: "SOR_029", // Coronet City
+            26: "SOR_030", // Jabba’s Palace
+        }[cardNumber] ?? ""
+    );
+}
 
 export function swuCardToSWUDBCardEntry(card: Card) {
     const {
@@ -18,10 +53,28 @@ export function swuCardToSWUDBCardEntry(card: Card) {
     let set = "SOR";
     switch (expansion) {
         case Expansion.SHD:
+            // swudb doesn't support SHD reprints
+            if (getSORReprint(cardNumber)) {
+                return {
+                    id: getSORReprint(cardNumber),
+                    count: 1,
+                };
+            }
+
+            // swudb doesn't support SHD bases
+            if (getSWUDBAlternative(cardNumber)) {
+                return {
+                    id: getSWUDBAlternative(cardNumber),
+                    count: 1,
+                };
+            }
+
             set = "SHD";
+            break;
 
         case Expansion.SOR:
         default:
+            set = "SOR";
     }
 
     const swudbCardEntry: SWUDBCardEntry = {
